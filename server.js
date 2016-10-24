7d7e69950534f765a22da71445b9e204b0d8e05e
Seth Coussens <seth.coussens@gmail.com>
2016-10-24 10:15:53 -0500

    no message

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 // server.js
 
 //setup
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose'); //mongoose for mongodb
-var morgan = require('morgan'); //log requests to the console
-var bodyParser = require('body-parser'); //pull information from HTML post
-var methodOverride = require('method-override'); //simulate DELETE and PUT
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose'); //mongoose for mongodb
+const morgan = require('morgan'); //log requests to the console
+const bodyParser = require('body-parser'); //pull information from HTML post
+const methodOverride = require('method-override'); //simulate DELETE and PUT
 
 //configuration
 var database = require('./config/database');
@@ -15,6 +16,7 @@ mongoose.connect(database.url); //connect to the db
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({'extended': 'true'}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
